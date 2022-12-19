b14a62e95a76e2685a69b8d05fe500cd4820846b
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2022-12-19 17:20:57 -0500

    add xml parsing thingy

--- common/xml.sh
+++ common/xml.sh
@@ -1,7 +1,7 @@
 xmatch() {
-	echo $1 
-	echo $2
-	echo ""
+	# echo $1 
+	# echo $2
+	# echo ""
 	xmlstarlet sel -t -m "$1" -m 'ancestor::*' -v 'name()' -o '/' \
 -b -v "concat(name(),'[@sink=\"',@sink, '\"]')" -n $2
     #
